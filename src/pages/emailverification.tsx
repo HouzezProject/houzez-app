e7f3219a9e09d4313ee9d77b41b89ad4ac1df5c7
jinzhih <jessie.houjinzhi@gmail.com>
2022-09-19 13:59:56 +1000

    HZ-6 layout and add some folders

---
+++ src/pages/emailverification.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import EmailVerification from "../components/EmailVerification";
+
+const EmailVerificationPage: NextPage = () => {
+  return <EmailVerification />;
+};
+export default EmailVerificationPage;
