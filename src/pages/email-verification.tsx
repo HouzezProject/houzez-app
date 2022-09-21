f3b88e45b03688ffb3dbae58d88cc34ef20bb106
jinzhih <jessie.houjinzhi@gmail.com>
2022-09-21 17:53:58 +1000

    Refactor: use style component

--- src/pages/email-verification.tsx
+++ src/pages/email-verification.tsx
@@ -1,38 +1,40 @@
-import * as React from "react";
+import styled from "@emotion/styled";
+import Image from "next/image";
+import IconImg from "../assets/icon/email.png";
 import type { NextPage } from "next";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import { Card, Container, Box, Typography } from "@mui/material";
+import theme from "../styles/theme";
+
+const {
+  palette: {
+    secondary: { light, main },
+    background: { paper: backgroundPaper }
+  }
+} = theme;
+
+const BoxEmailOpen = styled(Card)({
+  width: "550px",
+  height: "580px",
+  boxShadow: main + " 0 1px 5px 0px;",
+  marginTop: 16,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: backgroundPaper,
+  border: "1px solid " + light
+});
 
 const EmailVerificationPage: NextPage = () => {
   return (
     <>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Box
-          sx={{
-            width: "550px",
-            height: "580px",
-            boxShadow: 3,
-            marginTop: 16,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
-          }}
-          bgcolor="primary"
-        >
-          <Box
-            component="img"
-            sx={{
-              height: 100,
-              width: 100,
-              marginTop: 8
-            }}
-            alt="Email Open."
-            src="/images/email.png"
-          />
+        <BoxEmailOpen>
+          <Box mt="60px">
+            <Image src={IconImg} alt="IconImg" width="100px" height="100px" />
+          </Box>
           <Typography variant="h6" color="primary" mt="10px">
             Confirm Your Email
           </Typography>
@@ -46,7 +48,7 @@ const EmailVerificationPage: NextPage = () => {
           <Box mt="20px">
             <Button variant="contained">Resend verification email</Button>
           </Box>
-        </Box>
+        </BoxEmailOpen>
       </Container>
     </>
   );
