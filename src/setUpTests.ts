27e8e5b817bd4afe986dadc9bde301ad62c25fa6
Steven Dong <yuanningd@gmail.com>
2022-10-10 13:10:01 +1100

    update test config and eslint config

---
+++ src/setUpTests.ts
@@ -0,0 +1,5 @@
+// learn more: https://github.com/testing-library/jest-dom
+import { configure } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+configure({ asyncUtilTimeout: 2000 });
