1405b34327a5ec4238b40aae51226bddd1dbd79e
Tim <timguau@gmail.com>
2022-10-10 16:50:19 +1100

    HZ-0201 Create sign in page

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,89 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import Image from "next/image";
+import LogoImg from "../../assets/logo/logo_black.png";
+import SignUpInfo from "./SignInInfo";
+import SignUpThirdPart from "./SignInThirdParty";
+import { Card, Container, Divider, Typography } from "@mui/material";
+import Link from "next/link";
+import theme from "../../styles/theme";
+
+const {
+  palette: {
+    secondary: { light, main, dark },
+    background: { default: background, paper: backgroundPaper }
+  }
+} = theme;
+
+const SignUpContainer = styled(Container)({
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: background
+});
+
+const SignUpCard = styled(Card)({
+  minWidth: 500,
+  minHeight: 700,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "3px",
+  padding: "1.25rem",
+  border: "1px solid " + light,
+  fontSize: "1rem",
+  fontWeight: "400",
+  lineHeight: "1.5",
+  color: dark,
+  backgroundColor: backgroundPaper,
+  boxShadow: main + " 0 1px 5px 0px;"
+});
+
+const SignUpBox = styled(Box)({
+  width: "400px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "5px",
+  margin: "40px 0"
+});
+
+const SignUpDivider = styled(Divider)({
+  width: "100%",
+  fontWeight: "600"
+});
+
+const SignUp = () => {
+  return (
+    <SignUpContainer>
+      <SignUpCard>
+        <SignUpBox>
+          <Image src={LogoImg} alt="Houzez" width="200px" height="50px" />
+          <Typography variant="h4" mt="25px" mb="15px" sx={{ fontSize: "28px" }}>
+            Sign In
+          </Typography>
+
+          <SignUpInfo />
+
+          <Typography variant="body2" mt="10px" mb="10px" gap="10px">
+            <Link href="/forgot-password">Forgot your Password?</Link>
+          </Typography>
+
+          <SignUpDivider>OR</SignUpDivider>
+
+          <SignUpThirdPart />
+
+          <Typography variant="body2" mt="10px" mb="10px" gap="10px">
+            Not Signed up?
+            <Link href="/signup"> Create an account</Link>
+          </Typography>
+        </SignUpBox>
+      </SignUpCard>
+    </SignUpContainer>
+  );
+};
+
+export default SignUp;
---
+++ src/components/SignIn/SignInInfo.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./SignIn";
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import { Button, TextField } from "@mui/material";
+import theme from "../../styles/theme";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const {
+  palette: {
+    red: { main, dark }
+  }
+} = theme;
+
+const SignUpInfoTextField = styled(TextField)({
+  width: "100%",
+  height: "40px",
+  lineHeight: "30px",
+  marginBottom: "45px",
+  paddingLeft: "0",
+  letterSpacing: "0.05rem",
+  borderRadius: "3px",
+  fontSize: "1.5rem",
+  fontWeight: "400"
+});
+
+const SignUpInfoButton = styled(Button)({
+  width: "100%",
+  height: "50px",
+  marginTop: "10px",
+  backgroundColor: main,
+  "&:hover": {
+    backgroundColor: dark
+  }
+});
+
+const validationSchema = yup.object({
+  email: yup.string().email("Enter a valid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password should be of minimum 8 characters length")
+    .matches(/[A-Z]+/, "At least One uppercase character required")
+    .matches(/\d+/, "At least One number required")
+    .matches(/[@#$%^&+=]+/, "At least one special character required")
+    .required("Password is required")
+});
+
+const SignUpInfo = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    }
+  });
+
+  return (
+    <Box width="100%">
+      <form onSubmit={formik.handleSubmit} noValidate>
+        <SignUpInfoTextField
+          required
+          fullWidth
+          id="email"
+          type="email"
+          label="Email address"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlurCapture={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <SignUpInfoTextField
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <SignUpInfoButton variant="contained" type="submit">
+          Sign In
+        </SignUpInfoButton>
+      </form>
+    </Box>
+  );
+};
+
+export default SignUpInfo;
