02fee6cfca133db7489e2d7f7137334efd8801b6
Tim <timguau@gmail.com>
2022-10-12 00:38:36 +1100

    remove formik

--- src/components/SignIn/SignInInfo.tsx
+++ src/components/SignIn/SignInInfo.tsx
@@ -4,8 +4,6 @@ import styled from "@emotion/styled";
 import { Box } from "@mui/system";
 import { Button, TextField } from "@mui/material";
 import theme from "../../styles/theme";
-import { useFormik } from "formik";
-import * as yup from "yup";
 
 const {
   palette: {
@@ -35,47 +33,13 @@ const SignInInfoButton = styled(Button)({
   }
 });
 
-const validationSchema = yup.object({
-  email: yup.string().email("Enter a valid email").required("Email is required")
-});
-
 const SignInInfo = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: ""
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
-    }
-  });
-
   return (
     <Box width="100%">
-      <form onSubmit={formik.handleSubmit} noValidate>
-        <SignInInfoTextField
-          required
-          fullWidth
-          id="email"
-          type="email"
-          label="Email address"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlurCapture={formik.handleBlur}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <SignInInfoTextField
-          required
-          fullWidth
-          id="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-        />
-        <SignInInfoButton variant="contained" type="submit" disabled={Boolean(formik.errors.email)}>
+      <form noValidate>
+        <SignInInfoTextField required fullWidth id="email" type="email" label="Email address" />
+        <SignInInfoTextField required fullWidth id="password" label="Password" type="password" />
+        <SignInInfoButton variant="contained" type="submit">
           Sign In
         </SignInInfoButton>
       </form>
