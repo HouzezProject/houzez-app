45f8268ef02c09505de0125d6544b7e708721f9a
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2022-10-13 19:32:13 +1100

    update reset password UI

--- src/pages/password-reset.tsx
+++ src/pages/password-reset.tsx
@@ -1,7 +1,7 @@
-import { Box, Container, Button, Divider, InputBase, Typography } from "@mui/material";
+import { Box, Container, Button, Divider, InputBase, Typography, Card } from "@mui/material";
 import styled from "@mui/system/styled";
 import theme from "../styles/theme";
-import logo from "public/favicon.ico";
+import logo from "../../src/assets/logo/logo_black.png";
 import Image from "next/image";
 import { NextPage } from "next";
 import Link from "next/link";
@@ -9,151 +9,127 @@ import Link from "next/link";
 const {
   palette: {
     secondary: { light, main },
-    background: { paper: backgroundPaper },
+    background: { default: background, paper: backgroundPaper },
     red: { main: redMain, dark }
-  },
-  typography: {
-    fontFamily,
-    button: { textTransform }
   }
 } = theme;
 
 const ResetContainer = styled(Container)({
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: background
+});
+
+const ResetCard = styled(Card)({
+  minWidth: 500,
+  minHeight: 700,
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  borderRadius: "10px",
+  borderRadius: "3px",
   padding: "1.25rem",
-  border: "1px solid" + light,
+  border: "1px solid " + light,
   fontSize: "1rem",
   fontWeight: "400",
   lineHeight: "1.5",
-  flexDirection: "column",
-  gap: "25px",
+  color: dark,
   backgroundColor: backgroundPaper,
-  boxShadow: main + " 0 1px 5px 0px;",
-  marginTop: "100px"
+  boxShadow: main + " 0 1px 5px 0px;"
 });
 
-const HeaderBox = styled(Box)({
-  marginTop: "15px",
-  paddingLeft: "10px",
-  width: "50%",
+const ResetBox = styled(Box)({
+  borderRadius: "10px",
+  marginBottom: "2rem",
+  width: "400px",
   display: "flex",
-  gap: "10px",
-  textAlign: "center",
-  justifyItems: "center",
-  alignItems: "center"
-});
-
-const HouzezTypo = styled(Typography)({
-  fontFamily: fontFamily,
-  fontWeight: "500",
-  fontSize: "24px",
-  inlineHeight: "24px",
-  textAlign: "center",
-  alignItems: "center"
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px"
 });
 
 const InfoTypo = styled(Typography)({
-  fontFamily: fontFamily,
-  margin: "16px 50px 0 50px",
+  marginTop: "1rem",
+  marginBottom: "2rem",
   textAlign: "center",
   fontWeight: "600",
-  fontSize: "24px"
+  fontSize: "1.8rem",
+  color: "black"
 });
 const DetailTypo = styled(Typography)({
-  fontFamily: fontFamily,
-  margin: "16px 50px 0 50px",
+  marginBottom: "1rem",
   textAlign: "center",
   fontWeight: "400",
-  fontSize: "16px"
-});
-
-const CodeInputBase = styled(InputBase)({
-  fontFamily: fontFamily,
-  alignItems: "center",
-  backgroundColor: "white",
-  fontWeight: "400",
-  border: "1px solid" + light,
-  heigh: "2rem",
-  width: "70%",
-  padding: "6px 10px",
-  borderRadius: "5px",
-  boxShadow: "none",
-  marginTop: "0.5rem",
-  marginBottom: "0.5rem"
+  fontSize: "1.2rem",
+  color: "black"
 });
 
 const PasswordInputBase = styled(InputBase)({
-  fontFamily: fontFamily,
   alignItems: "center",
   backgroundColor: "white",
   fontWeight: "400",
   border: "1px solid" + light,
-  heigh: "2rem",
-  width: "70%",
+  width: "85%",
   padding: "6px 10px",
   borderRadius: "5px",
   boxShadow: "none",
-  marginBottom: "0.5rem"
+  marginBottom: "1rem"
 });
 
 const ConfirmInputBase = styled(InputBase)({
-  fontFamily: fontFamily,
   alignItems: "center",
   backgroundColor: "white",
   fontWeight: "400",
   border: "1px solid" + light,
-  heigh: "2rem",
-  width: "70%",
+  width: "85%",
   padding: "6px 10px",
   borderRadius: "5px",
   boxShadow: "none",
-  marginBottom: "0.5rem"
+  marginBottom: "1rem"
 });
 
 const SubmitButton = styled(Button)({
-  width: "70%",
-  heigh: "2rem",
-  fontWeight: "400",
+  width: "85%",
+  heigh: "50px",
+  inlineHeight: "20px",
   color: "white",
   padding: "6px 10px",
   backgroundColor: redMain,
   "&:hover": {
     backgroundColor: dark
   },
-  textTransform: textTransform
+  marginBottom: "1rem"
 });
 
 const ResetDivider = styled(Divider)({
+  margin: "15px",
   width: "100%",
   fontWeight: "600"
 });
 
 const RestPasswordPage: NextPage = () => {
   return (
-    <ResetContainer maxWidth="sm">
-      <HeaderBox>
-        <Image src={logo} alt="Houzez icon" height="50px" width="50px" />
-        <HouzezTypo variant="body2">Houzez.com.au</HouzezTypo>
-      </HeaderBox>
-      <InfoTypo variant="body1">Update your password</InfoTypo>
-      <DetailTypo variant="body2">
-        We have sent a verification code to your email. Enter the code here, with your new password details
-      </DetailTypo>
-      <CodeInputBase placeholder="Code" />
-      <PasswordInputBase placeholder="New password" />
-      <ConfirmInputBase placeholder="Confirm new password" />
-      <SubmitButton variant="contained">Change Password</SubmitButton>
-      <Typography variant="body2" mt="10px" mb="10px" gap="10px">
-        Go back to
-        <Link href="/signin"> sign in</Link>
-      </Typography>
-      <ResetDivider />
-      <Typography variant="body2" mt="10px" mb="10px">
-        Personal Information Collection Statement
-      </Typography>
+    <ResetContainer>
+      <ResetCard>
+        <ResetBox>
+          <Image src={logo} alt="Houzez" width="200px" height="50px"/>
+          <InfoTypo variant="body1">Reset your password</InfoTypo>
+          <DetailTypo variant="body2" mt="10px" mb="10px" gap="10px">
+            Enter your new password details
+          </DetailTypo>
+          <PasswordInputBase placeholder="New password" />
+          <ConfirmInputBase placeholder="Confirm new password" />
+          <SubmitButton variant="contained">Change Password</SubmitButton>
+          <ResetDivider />
+          <DetailTypo variant="body2">
+            Go back to
+            <Link href="/signin"> sign in</Link>
+          </DetailTypo>
+        </ResetBox>
+      </ResetCard>
     </ResetContainer>
   );
 };
