d207893a738a7acd143e8276231051ec55c3df0f
Ron Zhang <zhangqing6599@gmail.com>
2022-10-21 15:25:48 +1100

    change some files contents

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    image: {
+      unoptimized: true
+    } 
+  }
 
 module.exports = nextConfig
