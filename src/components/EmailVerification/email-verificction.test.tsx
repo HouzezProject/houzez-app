a5f86469e2c4596c7d27a7fb0972b99c78d31f45
Zachary1317 <zachary.zhao2@student.unimelb.edu.au>
2022-10-28 19:42:43 +1100

    update test code fix smells

--- src/components/EmailVerification/email-verificction.test.tsx
+++ src/components/EmailVerification/email-verificction.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, waitFor } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import EmailVerificationPage from "../../pages/email-verification";
 
