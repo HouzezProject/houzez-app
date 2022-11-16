d1437b1fd03ab4eb835053232bc5b8e84c8fec65
chester <chesterhu0008@gmail.com>
2022-11-16 12:29:10 +1100

    HZ0301 rebuild

--- src/components/AgentManagement/PropertyAdd/PropertyAddBody.tsx
+++ src/components/AgentManagement/PropertyAdd/PropertyAddBody.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-import {useForm} from "react-hook-form";
 import styled from "@emotion/styled";
 import {
+    AlertColor,
     Box,
     Checkbox,
     Divider,
@@ -22,18 +22,14 @@ import Autocomplete from "react-google-autocomplete";
 import Button from "@mui/material/Button";
 import ButtonGif from "../../../../public/assets/gif/lines.gif";
 import axiosClient from "../../../utils/axios";
-import {AxiosError} from "axios";
-
 
 const indoorCheckList = ["Ensuite", "Dishwasher", "Study", "Built in robes", "Alarm system", "Broadband", "Floorboards", "Gym", "Rumpus room", "Workshop"];
 const outdoorCheckList = ["Swimming pool", "Balcony", "Outdoor area", "Undercover parking", "Shed", "Fully fenced", "Outdoor spa", "Tennis court"];
 const typeCheckList = ["House", "Apartment", "Townhouse", "Villa", "Land", "Acreage", "Rural", "Block Of Units", "Retirement Living"]
 const stateCheckList = ["NSW", "VIC", "QSD", "SA", "WA", "TAS", "NT", "ACT"]
 
-
 const GridContainer = styled(Grid)({
     width: "100%",
-
 })
 
 const PropertyAddFormContainer = styled(Box)({
@@ -53,7 +49,6 @@ const PropertyAddFormContainer = styled(Box)({
         alignItems: "center",
     }
 });
-
 
 const FormTextField = styled(TextField)({
     width: "100%",
@@ -96,46 +91,80 @@ const AddImageButton = styled(Button)({
 
 });
 
+interface PropertyAttribute {
+    propertyType: string;
+    title: string;
+    price: number;
+    livingRoom: number;
+    bedroom: number;
+    bathroom: number;
+    garage: number;
+    landSize: number;
+    description: string;
+    state: string;
+    suburb: string;
+    street: string;
+    postcode: number;
+    preowned: boolean;
+    latitude: number;
+    longitude: number;
+    indoor: string[];
+    outdoor: string[];
+}
+
 const PropertyAddBody = () => {
 
+
+    const [propertyAttribute, setPropertyAttribute] = useState({} as PropertyAttribute);
+
+    const handleChange = (type: keyof PropertyAttribute) => (event: any) => {
+        setPropertyAttribute({...propertyAttribute, [type]: event.target.value});
+    };
+
     const {
-        register,
-        handleSubmit
-    } = useForm();
+        propertyType,
+        title,
+        price,
+        livingRoom,
+        bedroom,
+        bathroom,
+        garage,
+        landSize,
+        description,
+        state,
+        suburb,
+        street,
+        postcode,
+        preowned,
+        latitude,
+        longitude,
+        indoor,
+        outdoor
+    } = propertyAttribute;
 
-    const [data, setData] = useState("");
-
-    const onSubmit = async (data) => {
+    const onSubmit = async () => {
         try {
+            // let list = "";
+            // propertyAttribute.indoor.forEach((data: String) => {
+            //     list += (data + ",");
+            // });
+            // propertyAttribute.indoor = list;
+            //
+            // list = "";
+            // propertyAttribute.outdoor.forEach((data: String) => {
+            //     list += (data + ",");
+            // });
+            // propertyAttribute.outdoor = list;
 
-            setData(JSON.stringify(data));
-
-            let list = "";
-            data.indoor.forEach((data: String) => {
-                list += (data + ",");
-            });
-            data.indoor = list;
-
-            list = "";
-            data.outdoor.forEach((data: String) => {
-                list += (data + ",");
-            });
-            data.outdoor = list;
-
-            await axiosClient.post("/agents/1/properties", data);
+            await axiosClient.post("/agents/1/properties", propertyAttribute);
         } catch (error) {
-            if (error instanceof AxiosError && error.response?.status === 400) {
-                //setAccountActive({severity: "error", display: "flex", text: error.response.data.details});
-            }
+
         }
     }
 
     return (
-
         <PropertyAddFormContainer>
-            <form noValidate onSubmit={handleSubmit(onSubmit)}>
-
-
+            <form noValidate onSubmit={onSubmit}>
                 <GridContainer container spacing={3}>
                     <Grid item xs={12}>
                         <AddPropertyDivider/>
@@ -143,23 +172,14 @@ const PropertyAddBody = () => {
                     <Grid item xs={12}>
                         <LevelTwoTitle>Basic Information</LevelTwoTitle>
                     </Grid>
-                    <Grid item xs={6}>
-                        <FormTextField id="title" label="Title" size="small" {...register("title")}/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <FormControl fullWidth size="small">
-                            <InputLabel id="type">Type</InputLabel>
-                            <Select labelId="type" id="type" label="Type" {...register("propertyType")}>
-                                {typeCheckList.map((item) => (
-                                    <MenuItem value={item.toUpperCase().replaceAll(" ", "_")}>{item}</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormTextField id="description" multiline rows={4} label="Description"
-                                       size="small" {...register("description")}/>
-                    </Grid>
+                    <PropertyAddInput xs={6} label="Title" value={title} attribute="title"
+                                      handleChange={handleChange} multiline={false}/>
+
+                    <AddPropertyDropDown xs={6} labelId="type" label="Type" value={propertyType}
+                                         handleChange={handleChange} attribute={"propertyType"}
+                                         checklist={typeCheckList}/>
+                    <PropertyAddInput xs={12} label="Description" value={description} attribute="description"
+                                      handleChange={handleChange} multiline={true} rows={4}/>
                     <Grid item xs={12}>
                         <AddPropertyDivider/>
                     </Grid>
@@ -170,97 +190,84 @@ const PropertyAddBody = () => {
                     <Grid item xs={3}>
                         <FormTextField id="price" type="number" label="Price" size="small" InputProps={{
                             startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                        }} {...register("price")}/>
+                        }} value={propertyAttribute.price} onChange={handleChange("price")}/>
                     </Grid>
+
                     <Grid item xs={3}>
                         <FormTextField id="land-size" type="number" label="Land Size" size="small" InputProps={{
                             startAdornment: <InputAdornment position="start">m²</InputAdornment>,
-                        }}  {...register("landSize")}/>
+                        }} value={propertyAttribute.landSize} onChange={handleChange("landSize")}/>
                     </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="bedroom" type="number" label="Bedroom"
-                                       size="small"  {...register("bedroom")}/>
-                    </Grid>
+                    <PropertyAddInput type="number" xs={3} label="Bedroom" value={bedroom}
+                                      handleChange={handleChange} attribute="bedroom" multiline={false}/>
                     <Grid item xs={3}>
                         <FormControlLabel
                             control={
-                                <Switch  {...register("preowned")}/>
+                                <Switch value={propertyAttribute.preowned} onChange={handleChange("preowned")}/>
                             }
                             label="preowned"
                         />
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs={12}>
                         <Input
                             fullWidth
-                            inputComponent={({onFocus, onBlur, ...props}) => (
+                            inputComponent={({...props}) => (
                                 <Autocomplete
                                     apiKey={"AIzaSyDRsNEvVJiPnDeu6geyVIwOs3x1yHIzngs"}
                                     {...props}
                                     options={{
                                         types: [],
                                         componentRestrictions: {country: "au"},
-                                        fields: ["address_components", "geometry", "icon", "name"],
+                                        fields: ["address_components", "geometry"],
                                     }}
                                     language={"en"}
-                                    onPlaceSelected={(selected) => {
-                                        console.log(selected);
-                                        document.getElementById("suburb").value = selected.address_components[0].long_name;
-                                        document.getElementById("state").value = Object.keys(selected.address_components[1].short_name);
+                                    onPlaceSelected={({address_components, geometry}) => {
+                                        console.log(geometry)
+                                        setPropertyAttribute({
+                                            ...propertyAttribute,
+                                            street: address_components[0].long_name + " " + address_components[1].long_name,
+                                            suburb: address_components[2].long_name,
+                                            state: address_components[4].short_name,
+                                            postcode: address_components[6].short_name
+                                        });
                                     }}
                                 />
                             )}
                         />
                     </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="suburb" label="Suburb" size="small"
-                                       InputLabelProps={{shrink: true}} {...register("suburb")}/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormControl fullWidth size="small">
-                            <InputLabel id="state">State</InputLabel>
-                            <Select labelId="state" id="state" label="State" {...register("state")}>
-                                {stateCheckList.map((item) => (
-                                    <MenuItem value={item}>{item}</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="postcode" type="number" label="Postcode"
-                                       size="small" {...register("postcode")}/>
-                    </Grid>
+                    <PropertyAddInput type="text" xs={3} label="Street" value={street} handleChange={handleChange}
+                                      attribute="street" multiline={false}/>
+                    <PropertyAddInput type="text" xs={3} label="Suburb" value={suburb} handleChange={handleChange}
+                                      attribute="suburb" multiline={false}/>
+                    <PropertyAddInput type="text" xs={3} label="State" value={state} handleChange={handleChange}
+                                      attribute="state" multiline={false}/>
+                    <PropertyAddInput type="number" xs={3} label="Postcode" value={postcode}
+                                      handleChange={handleChange} attribute="postcode" multiline={false}/>
                     <Grid item xs={12}>
                         <AddPropertyDivider/>
                     </Grid>
-
                     <Grid item xs={12}>
                         <LevelTwoTitle>Dimensions</LevelTwoTitle>
                     </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="living-room" type="number" label="Living room"
-                                       size="small" {...register("livingRoom")}/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="bathroom" type="number" label="Bathroom"
-                                       size="small" {...register("bathroom")}/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <FormTextField id="garage" type="number" label="Garage" size="small"  {...register("garage")}/>
-                    </Grid>
+                    <PropertyAddInput type="number" xs={3} label="Living room" value={livingRoom}
+                                      handleChange={handleChange} attribute="livingRoom" multiline={false}/>
+                    <PropertyAddInput type="number" xs={3} label="Bathroom" value={bathroom}
+                                      handleChange={handleChange} attribute="bathroom" multiline={false}/>
+                    <PropertyAddInput type="number" xs={3} label="Garage" value={garage}
+                                      handleChange={handleChange} attribute="garage" multiline={false}/>
                     <Grid item xs={12}>
                         <AddPropertyDivider/>
                     </Grid>
-
                     <Grid item xs={12}>
                         <LevelTwoTitle>General Amenities</LevelTwoTitle>
                     </Grid>
                     <Grid item xs={12}>
                         <LevelThreeTitle>Indoor</LevelThreeTitle>
                     </Grid>
-
                     {indoorCheckList.map((item, index) => (
                         <Grid item xs={1.5} key={index}>
-                            <FormControlLabel control={<Checkbox size={"small"} value={item} {...register("indoor")}/>}
+                            <FormControlLabel control={<Checkbox size={"small"} value={propertyAttribute.indoor}
+                                                                 onChange={handleChange("indoor")}/>}
                                               label={item}/>
                         </Grid>
                     ))}
@@ -269,7 +276,8 @@ const PropertyAddBody = () => {
                     </Grid>
                     {outdoorCheckList.map((item, index) => (
                         <Grid item xs={1.5} key={index}>
-                            <FormControlLabel control={<Checkbox size={"small"} value={item} {...register("outdoor")}/>}
+                            <FormControlLabel control={<Checkbox size={"small"} value={propertyAttribute.outdoor}
+                                                                 onChange={handleChange("outdoor")}/>}
                                               label={item}/>
                         </Grid>
                     ))}
@@ -279,12 +287,56 @@ const PropertyAddBody = () => {
                     <Grid item xs={11}/>
                     <Grid item xs={1}>
                         <SubmitButton type={"submit"} variant="contained">Submit</SubmitButton>
-                    </Grid>                <p>{data.toString()}</p>
+                    </Grid>
 
                 </GridContainer>
+
             </form>
         </PropertyAddFormContainer>
     );
 };
+
+const PropertyAddInput = ({
+                              xs,
+                              type = "text",
+                              label,
+                              handleChange,
+                              value,
+                              attribute,
+                              multiline,
+                              rows = 1
+                          }: { xs: number, type?: string, label: string, value: string | number, handleChange: (type: keyof PropertyAttribute) => (event: any) => void, attribute: keyof PropertyAttribute, multiline: boolean, rows?: number }) => {
+    return (
+        <Grid item xs={xs}>
+            <FormTextField type={type} label={label} multiline={multiline} rows={rows}
+                           size="small" value={value}
+                           onChange={handleChange(attribute)}/>
+        </Grid>
+    )
+}
+
+const AddPropertyDropDown = ({
+                                 xs,
+                                 labelId,
+                                 label,
+                                 value,
+                                 handleChange,
+                                 attribute,
+                                 checklist
+                             }: { xs: number, labelId: string, label: string, value: string, handleChange: (type: keyof PropertyAttribute) => (event: any) => void, attribute: keyof PropertyAttribute, checklist: string[] }) => {
+    return (
+        <Grid item xs={xs}>
+            <FormControl fullWidth size="small">
+                <InputLabel id={labelId}>Type</InputLabel>
+                <Select labelId={labelId} label={label} value={value}
+                        onChange={handleChange(attribute)}>
+                    {checklist.map((item) => (
+                        <MenuItem value={item.toUpperCase().replaceAll(" ", "_")}>{item}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </Grid>
+    )
+}
 
 export default PropertyAddBody;
